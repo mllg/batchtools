afe5f93ced801459facdb935cdeb90f182e54b72
Michel <michellang@gmail.com>
2015-10-27 14:50:57 +0100

    exported summarizeExperiments

--- R/summarizeExperiments.r
+++ R/summarizeExperiments.r
@@ -1,4 +1,15 @@
+#' @title Quick Summary over Experiments
+#'
+#' @description
+#' Returns a frequency table of defined experiments.
+#'
+#' @templateVar ids.default all
+#' @template ids
+#' @param by [\code{character}]\cr
+#'   Split the resulting table by these columns of \code{\link{getJobDefs}}.
+#' @template reg
+#' @return [\code{\link[base]{data.table}}] of frequencies in the respective groups.
 summarizeExperiments = function(ids = NULL, by = c("problem", "algorithm"), reg = getDefaultRegistry()) {
   pars = !setequal(by, c("problem", "algorithm"))
-  getJobInfo(ids = ids, pars.as.cols = pars, reg = reg)[, list(.count = .N), by = by]
+  getJobDefs(ids = ids, pars.as.cols = pars, reg = reg)[, list(.count = .N), by = by]
 }
