3a7f92db31fcb226679c1e07a8b49f5c6cf74b70
Michel <michellang@gmail.com>
2015-11-03 13:50:09 +0100

    added test for waitForJobs

--- tests/testthat/test_waitForJobs.r
+++ tests/testthat/test_waitForJobs.r
@@ -4,7 +4,18 @@ test_that("waitForJobs", {
   reg = makeTempRegistry(make.default = FALSE)
   fun = function(x) if (x == 2) stop(x) else x
   ids = batchMap(reg = reg, fun, 1:2)
-  submitJobs(reg = reg, ids = ids)
+  submitJobs(ids, reg = reg)
   expect_true(waitForJobs(ids = ids[1], reg = reg))
   expect_false(waitForJobs(ids = ids, stop.on.error = TRUE, reg = reg))
+})
+
+test_that("waitForJobs: detection of expired jobs", {
+  skip_on_cran()
+  reg = makeTempRegistry(make.default = FALSE)
+  if (is.null(reg$cluster.functions$killJobs))
+    skip("Test requires killJobs")
+  ids = batchMap(reg = reg, Sys.sleep, c(20, 20))
+  submitJobs(ids, reg = reg)
+  killJobs(1, reg = reg)
+  expect_false(expect_warning(waitForJobs(ids, reg = reg)))
 })
