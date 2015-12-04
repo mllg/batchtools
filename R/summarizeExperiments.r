4548f20d089a402e9e56c224f758dc5277bbba19
Michel <michellang@gmail.com>
2015-12-04 16:02:56 +0100

    return problems and algorithms in summarizeExperiments

--- R/summarizeExperiments.r
+++ R/summarizeExperiments.r
@@ -8,10 +8,22 @@
 #' @param by [\code{character}]\cr
 #'   Split the resulting table by columns of \code{\link{getJobPars}}.
 #' @template reg
-#' @return [\code{\link[data.table]{data.table}}] of frequencies in the respective groups.
+#' @return [\code{named list}] with elements \dQuote{table}
+#' (\code{\link[data.table]{data.table}}] of frequencies), \dQuote{problems}
+#' (\code{character} of problem names) and \dQuote{algorithms}
+#' (\code{character} of algorithm names).
 #' @export
 summarizeExperiments = function(ids = NULL, by = c("problem", "algorithm"), reg = getDefaultRegistry()) {
   assertExperimentRegistry(reg)
   pars = !setequal(by, c("problem", "algorithm"))
-  getJobDefs(ids = ids, pars.as.cols = pars, reg = reg)[, list(.count = .N), by = by]
+  setClasses(list(
+    problems = levels(reg$defs$problem),
+    algorithms = levels(reg$defs$algorithm),
+    table = getJobDefs(ids = ids, pars.as.cols = pars, reg = reg)[, list(.count = .N), by = by]
+  ), "ExperimentSummary")
+}
+
+#' @export
+print.ExperimentSummary = function(x, ...) {
+  print(x$table)
 }
