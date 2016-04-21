4381f4ac70ea22a683f1288cf10cdca5bc5ea0b7
Michel <michellang@gmail.com>
2016-04-21 22:35:43 +0200

    re-enabled testJob tests

--- tests/testthat/test_testJob.R
+++ tests/testthat/test_testJob.R
@@ -5,10 +5,10 @@ test_that("testJob", {
   f = function(x) if (x %% 2 == 0) stop("foo") else x^2
   batchMap(reg = reg, f, 1:3)
   expect_equal(testJob(reg = reg, id = 1), 1)
-  # expect_equal(suppressAll(testJob(reg = reg, id = 1, fresh.session = TRUE)), 1)
+  expect_equal(suppressAll(testJob(reg = reg, id = 1, fresh.session = TRUE)), 1)
   expect_equal(testJob(reg = reg, id = 3), 9)
   expect_error(testJob(reg = reg, id = 2), "foo")
-  # expect_error(suppressAll(testJob(reg = reg, id = 2, fresh.session = TRUE)), "re-run")
+  expect_error(suppressAll(testJob(reg = reg, id = 2, fresh.session = TRUE)), "re-run")
 
   expect_equal(findSubmitted(reg = reg), data.table(job.id = integer(0L), key = "job.id"))
   expect_equal(findDone(reg = reg), data.table(job.id = integer(0L), key = "job.id"))
@@ -16,13 +16,6 @@ test_that("testJob", {
 })
 
 test_that("testJob.ExperimentRegistry", {
-  suppressAll = function (expr) {
-    invisible(capture.output({
-      suppressWarnings(suppressMessages(suppressPackageStartupMessages(x <- force(expr))))
-    }))
-    return(x)
-  }
-
   reg = makeExperimentRegistry(file.dir = NA, make.default = FALSE)
   prob = addProblem(reg = reg, "p1", data = iris, fun = function(job, data, ...) nrow(data), seed = 42)
   algo = addAlgorithm(reg = reg, "a1", fun = function(job, data, instance, sq, ...) instance^sq)
@@ -30,6 +23,6 @@ test_that("testJob.ExperimentRegistry", {
 
   suppressAll(x <- testJob(id = 1, reg = reg))
   expect_equal(x, 150)
-  suppressAll(x <- testJob(id = 2, reg = reg))
+  suppressAll(x <- testJob(id = 2, reg = reg, fresh.session = TRUE))
   expect_equal(x, 150^2)
 })
