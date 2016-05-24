aee1341ff15d30ffcd47fe3fce34136a4460c31e
Michel Lang <michellang@gmail.com>
2016-05-24 09:57:19 +0200

    improved testJob

--- R/testJob.R
+++ R/testJob.R
@@ -4,12 +4,12 @@
 #' Starts a single job on the local machine.
 #'
 #' @template id
-#' @param fresh.session [\code{logical(1)}]\cr
-#'  Run the job in a fresh R session? If \code{TRUE}, starts a fresh R
+#' @param external [\code{logical(1)}]\cr
+#'  Run the job in an external R session? If \code{TRUE}, starts a fresh R
 #'  session on the local machine to execute the with \code{\link{execJob}}.
 #'  You will not be able to use debug tools like \code{\link[base]{traceback}}
 #'  or \code{\link[base]{browser}}.
-#'  If \code{fresh.session} is set to \code{FALSE} (default), \code{testJob} will
+#'  If \code{external} is set to \code{FALSE} (default), \code{testJob} will
 #'  execute the job in the current R session, and you are unable to spot
 #'  missing variable declarations (possibly resolved in the global environment) as well
 #'  as forgotten package dependencies.
@@ -24,15 +24,15 @@
 #' \dontrun{
 #'  testJob(2, reg = reg)
 #' }
-testJob = function(id, fresh.session = FALSE, reg = getDefaultRegistry()) {
+testJob = function(id, external = FALSE, reg = getDefaultRegistry()) {
   assertRegistry(reg)
-  assertFlag(fresh.session)
+  assertFlag(external)
   id = asJobTable(reg, id)
   if (nrow(id) != 1L)
     stopf("You must provide exactly 1 id (%i provided)", nrow(id))
   job = makeJob(id, reg = reg)
 
-  if (fresh.session) {
+  if (external) {
     fn      = npath(tempfile("testjob_"))
     fn.r    = stri_join(fn, ".R")
     fn.job  = stri_join(fn, ".job")
@@ -47,7 +47,8 @@ testJob = function(id, fresh.session = FALSE, reg = getDefaultRegistry()) {
       writeLines(res$output)
       return(readRDS(fn.res))
     } else {
-      stopf("testJob() failed for job with id=%i. To properly debug, re-run with fresh.session = FALSE", id$job.id)
+      writeLines(res$output)
+      stopf("testJob() failed for job with id=%i. To properly debug, re-run with external=FALSE", id$job.id)
     }
   } else {
     loadRegistryDependencies(reg, switch.wd = TRUE)
