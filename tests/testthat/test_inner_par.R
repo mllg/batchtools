e47d8653b49ed4c60ca00cd8e3bab9f66304bfca
Michel Lang <michellang@gmail.com>
2016-05-31 11:06:32 +0200

    improved inner par test

--- tests/testthat/test_inner_par.R
+++ tests/testthat/test_inner_par.R
@@ -7,7 +7,7 @@ ids = batchMap(fun, i = 1:4, reg = reg)
 test_that("chunk/socket", {
   skip_if_not_installed("snow")
   silent({
-    submitJobs(chunkIds(ids, reg = reg), resources = list(inner.mode = "chunk", inner.ncpus = 2, inner.backend = "socket"), reg = reg)
+    submitJobs(chunkIds(ids, reg = reg), resources = list(inner.mode = "chunk", inner.backend = "socket"), reg = reg)
     waitForJobs(reg = reg)
   })
   expect_true(nrow(findDone(reg = reg)) == 4)
@@ -16,7 +16,7 @@ test_that("chunk/socket", {
 test_that("chunk/multicore", {
   skip_on_os("windows")
   silent({
-    submitJobs(chunkIds(ids, reg = reg), resources = list(inner.mode = "chunk", inner.ncpus = 2, inner.backend = "multicore"), reg = reg)
+    submitJobs(chunkIds(ids, reg = reg), resources = list(inner.mode = "chunk", inner.backend = "multicore"), reg = reg)
     waitForJobs(reg = reg)
   })
   expect_true(nrow(findDone(reg = reg)) == 4)
@@ -27,7 +27,7 @@ test_that("chunk/Rmpi", {
   skip_on_cran()
   skip_on_travis()
   silent({
-    submitJobs(chunkIds(ids, reg = reg), resources = list(inner.mode = "chunk", inner.ncpus = 2, inner.backend = "mpi"), reg = reg)
+    submitJobs(chunkIds(ids, reg = reg), resources = list(inner.mode = "chunk", inner.backend = "mpi"), reg = reg)
     waitForJobs(reg = reg)
   })
   expect_true(nrow(findDone(reg = reg)) == 4)
