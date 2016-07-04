6e8b6a54e57afe19f89c5f18c6d2e7ff389e5c21
Michel Lang <michellang@gmail.com>
2016-07-05 00:26:30 +0200

    added resetJobs (fixes #18)

---
+++ R/resetJobs.R
@@ -0,0 +1,25 @@
+#' @title Reset the Computational State of Jobs
+#'
+#' @description
+#' Resets the state of jobs in the \code{\link{Registry}}.
+#' This function automatically checks if any of the jobs to reset is either pending or running.
+#' However, if the implemented heuristic fails, this can lead to inconsistencies in the data base.
+#' Use with care.
+#'
+#' @templateVar ids.default none
+#' @template ids
+#' @template reg
+#' @return [\code{\link{data.table}}] of job ids which have been reset.
+#'   See \code{\link{JoinTables}} for examples on working with job tables.
+#' @family debug
+#' @export
+resetJobs = function(ids, reg = getDefaultRegistry()) {
+  assertRegistry(reg, writeable = TRUE, running.ok = FALSE)
+  ids = asJobTable(reg, ids)
+
+  info("Resetting %i jobs in DB.", nrow(ids))
+  reg$status[ids, c("submitted", "started", "done", "error", "memory", "resource.id", "batch.id", "job.hash") := list(NA_integer_, NA_integer_, NA_integer_, NA_character_, NA_real_, NA_integer_, NA_character_, NA_character_), with = FALSE]
+
+  sweepRegistry(reg)
+  invisible(ids)
+}
---
+++ tests/testthat/test_resetJobs.R
@@ -0,0 +1,28 @@
+context("resetJobs")
+
+test_that("resetJobs", {
+  reg = makeRegistry(file.dir = NA, make.default = FALSE)
+  f = function(x) if (x == 2) stop(2) else x
+  batchMap(f, 1:3, reg = reg)
+  before = list(
+    status = copy(reg$status),
+    defs = copy(reg$defs)
+  )
+  submitJobs(chunkIds(1:3, reg = reg), reg = reg)
+
+  expect_true(file.exists(file.path(reg$file.dir, "logs", sprintf("%s.log", reg$status[.(3), job.hash]))))
+  expect_false(identical(reg$status$submitted, before$status$submitted))
+  expect_true(file.exists(file.path(reg$file.dir, "results", "1.rds")))
+
+  resetJobs(1, reg = reg)
+  expect_true(all.equal(before$status[1], reg$status[1]))
+  expect_false(file.exists(file.path(reg$file.dir, "results", "1.rds")))
+  expect_true(file.exists(file.path(reg$file.dir, "results", "3.rds")))
+  expect_true(file.exists(file.path(reg$file.dir, "logs", sprintf("%s.log", reg$status[.(3), job.hash]))))
+  expect_character(all.equal(before$status, reg$status))
+
+  resetJobs(2:3, reg = reg)
+  expect_true(all.equal(before$status, reg$status))
+  expect_false(file.exists(file.path(reg$file.dir, "logs", sprintf("%s.log", reg$status[.(3), job.hash]))))
+  expect_false(file.exists(file.path(reg$file.dir, "results", "3.rds")))
+})
