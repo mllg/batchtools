f14ab477dbed684e204e88a104aa634b7227b6d7
Michel Lang <michellang@gmail.com>
2017-01-10 16:52:10 +0100

    remove obsolete result files after removeExperiments

--- R/removeExperiments.R
+++ R/removeExperiments.R
@@ -18,12 +18,13 @@ removeExperiments = function(ids = NULL, reg = getDefaultRegistry()) {
 
   info("Removing %i Experiments ...", nrow(ids))
   reg$status = reg$status[!ids]
-
   i = reg$defs[!reg$status, on = "def.id", which = TRUE]
   if (length(i) > 0L) {
     info("Cleaning up %i job definitions ...", length(i))
     reg$defs = reg$defs[-i]
   }
+  fns = getResultFiles(reg$file.dir, ids$job.id)
+  file.remove(fns[file.exists(fns)])
 
   sweepRegistry(reg)
   return(ids)
