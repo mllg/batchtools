a6a376256e6fd3dc8652a1ecdfb0becc701cb72d
Michel Lang <michellang@gmail.com>
2017-03-22 13:00:45 +0100

    improved examples: addExperiments and reduceResults (fixes #57)

--- R/reduceResults.R
+++ R/reduceResults.R
@@ -100,11 +100,49 @@ reduceResults = function(fun, ids = NULL, init, ..., reg = getDefaultRegistry())
 #' @family Results
 #' @export
 #' @examples
+#' ### Example 1 - reduceResultsList
 #' tmp = makeRegistry(file.dir = NA, make.default = FALSE)
 #' batchMap(function(x) x^2, x = 1:10, reg = tmp)
 #' submitJobs(reg = tmp)
 #' waitForJobs(reg = tmp)
 #' reduceResultsList(fun = sqrt, reg = tmp)
+#'
+#' ### Example 2 - reduceResultsDataTable
+#' tmp = makeExperimentRegistry(file.dir = NA, make.default = FALSE)
+#'
+#' # add first problem
+#' fun = function(job, data, n, mean, sd, ...) rnorm(n, mean = mean, sd = sd)
+#' addProblem("rnorm", fun = fun, reg = tmp)
+#'
+#' # add second problem
+#' fun = function(job, data, n, lambda, ...) rexp(n, rate = lambda)
+#' addProblem("rexp", fun = fun, reg = tmp)
+#'
+#' # add first algorithm
+#' fun = function(instance, method, ...) if (method == "mean") mean(instance) else median(instance)
+#' addAlgorithm("average", fun = fun, reg = tmp)
+#'
+#' # add second algorithm
+#' fun = function(instance, ...) sd(instance)
+#' addAlgorithm("deviation", fun = fun, reg = tmp)
+#'
+#' # define problem and algorithm designs
+#' prob.designs = algo.designs = list()
+#' prob.designs$rnorm = expand.grid(n = 100, mean = -1:1, sd = 1:5)
+#' prob.designs$rexp = data.table(n = 100, lambda = 1:5)
+#' algo.designs$average = data.table(method = c("mean", "median"))
+#' algo.designs$deviation = data.table()
+#'
+#' # add experiments and submit
+#' addExperiments(prob.designs, algo.designs, reg = tmp)
+#' submitJobs(reg = tmp)
+#'
+#' # collect results and join them
+#' # with problem and algorithm paramters
+#' ijoin(
+#'   getJobPars(reg = tmp),
+#'   reduceResultsDataTable(reg = tmp, fun = function(x) list(res = x))
+#' )
 reduceResultsList = function(ids = NULL, fun = NULL, ..., missing.val, reg = getDefaultRegistry()) {
   assertRegistry(reg, sync = TRUE)
   assertFunction(fun, null.ok = TRUE)
