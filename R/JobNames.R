0e2878cef31a0213181707efc72eb4c690883fa5
Michel Lang <michellang@gmail.com>
2017-07-25 09:43:22 +0200

    docs

--- R/JobNames.R
+++ R/JobNames.R
@@ -1,10 +1,11 @@
-#' @title Set Custom Job Names
+#' @title Set and Retrieve Job Names
+#' @name JobNames
 #'
 #' @description
 #' Set custom names for jobs. These are passed to the template as
 #' \sQuote{job.name}.
 #'
-#' @templateVar ids.default none
+#' @templateVar ids.default all
 #' @template ids
 #' @param names [\code{character}]\cr
 #'  Character vector of the same length as provided ids.
@@ -15,11 +16,20 @@
 #' tmp = makeRegistry(file.dir = NA, make.default = FALSE)
 #' ids = batchMap(identity, 1:10, reg = tmp)
 #' setJobNames(ids, letters[1:nrow(ids)], reg = tmp)
-setJobNames = function(ids, names, reg = getDefaultRegistry()) {
+#' getJobNames(reg = tmp)
+setJobNames = function(ids = NULL, names, reg = getDefaultRegistry()) {
   assertRegistry(reg, writeable = TRUE)
-  ids = convertIds(reg, ids)
+  ids = convertIds(reg, ids, default = noIds())
   assertCharacter(names, min.chars = 1L, len = nrow(ids), pattern = "^[[:alnum:]_.-]+$")
 
   reg$status[ids, job.names := names]
   saveRegistry(reg)
+}
+
+#' @export
+#' @rdname JobNames
+getJobNames = function(ids = NULL, reg = getDefaultRegistry()) {
+  assertRegistry(reg)
+  ids = convertIds(reg, ids, default = allIds(reg))
+  reg$status[ids, c("job.id", "job.name")]
 }
